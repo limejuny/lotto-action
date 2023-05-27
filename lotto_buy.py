edbb723de1057e5760c0cfdc3810e11be3557d09
Junyeong Yim <lime@juny.me>
2023-05-28 01:06:26 +0900

    로또 구매 액션 불필요한 의존성 제거
    Detail
    ======
     -

--- lotto_buy.py
+++ lotto_buy.py
@@ -1,23 +1,26 @@
-from playwright.sync_api import Playwright, sync_playwright
-import time
 import os
+import time
+
+import requests
+from playwright.sync_api import sync_playwright
 
 # 동행복권 아이디와 패스워드를 설정
 USER_ID = os.environ.get('USER_ID')
 USER_PW = os.environ.get('USER_PW')
 
+DISCORD_WEBHOOK_ID = os.environ.get('DISCORD_WEBHOOK_ID')
+DISCORD_WEBHOOK_TOKEN = os.environ.get('DISCORD_WEBHOOK_TOKEN')
+
 # 구매 개수를 설정
 COUNT = 5
 
-
-def run(playwright: Playwright) -> None:
+with sync_playwright() as playwright:
 
     # chrome 브라우저를 실행
     browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
 
     # Open new page
-    page = context.new_page()
+    page = browser.new_page()
 
     # Go to https://dhlottery.co.kr/user.do?method=login
     page.goto("https://dhlottery.co.kr/user.do?method=login")
@@ -71,10 +74,30 @@ def run(playwright: Playwright) -> None:
     page.click("input[name=\"closeLayer\"]")
     # assert page.url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
 
+    # 잔액 조회
+    page.goto("https://dhlottery.co.kr/userSsl.do?method=myPage")
+    balance = page.query_selector("p.total_new > strong")
+    table = page.query_selector(
+        "table.tbl_data.tbl_data_col > tbody > tr:nth-child(1)")
+    if balance and table:
+        date = table.query_selector("td:nth-child(1)")
+        rnd = table.query_selector("td:nth-child(2)")
+        if date and rnd:
+            data = {
+                "username":
+                    f"로또6/45",
+                "embeds": [{
+                    "title":
+                        f"{rnd.inner_text()}회차 구매",
+                    "description":
+                        f"구매일: {date.inner_text()}\n잔액: {balance.inner_text()}원"
+                }]
+            }
+            requests.post(
+                f"https://discord.com/api/webhooks/{DISCORD_WEBHOOK_ID}/{DISCORD_WEBHOOK_TOKEN}",
+                json=data)
+    else:
+        print("None")
+
     # ---------------------
-    context.close()
     browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
